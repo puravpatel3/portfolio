146e2f54b5523d5f0e17de109f0a0d9d411dbaa6
puravpatel3 <166936464+puravpatel3@users.noreply.github.com>
2024-10-03 15:01:18 -0400

    Update project_eda.py

--- pages/project_eda.py
+++ pages/project_eda.py
@@ -16,6 +16,11 @@ This app demonstrates my expertise in exploratory data analysis (EDA), providing
 Users can preview their dataset, explore field relationships, review summary statistics, and create visualizations based on their data. EDA is a critical step in any data science workflow and forms the foundation of insightful data-driven decision-making.
 """)
 
+# Data Source
+st.markdown("""
+**Data Source**: [Electric Vehicle Charging Patterns Dataset on Kaggle](https://www.kaggle.com/datasets/valakhorasani/electric-vehicle-charging-patterns)
+""")
+
 # Adding Instructions
 st.subheader("Instructions")
 st.write("""
