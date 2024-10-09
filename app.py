f7ef4cf4d2db8df60cf39eb8612aee9bfd6efaed
puravpatel3 <166936464+puravpatel3@users.noreply.github.com>
2024-10-09 09:28:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,26 +11,9 @@ def main():
     # Left align the text under the title
     st.markdown("""
     <div style='text-align: left'>
-        Use the buttons below to navigate through my profiles and connect with me.
+        Use the links below to view my LinkedIn profile and Resume.
     </div>
     """, unsafe_allow_html=True)
-
-    # Buttons section (organized horizontally below the title)
-    linkedin_url = "https://www.linkedin.com/in/puravp"
-    resume_file = 'https://raw.githubusercontent.com/puravpatel3/portfolio/main/files/Purav_Patel_Resume.pdf'
-    contact_email = "mailto:patel.a.purav@gmail.com"
-
-    # Creating horizontal layout for the buttons
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        st.write(f'<a href="{linkedin_url}" target="_blank"><button class="btn btn-primary">LinkedIn Profile</button></a>', unsafe_allow_html=True)
-
-    with col2:
-        st.write(f'<a href="{resume_file}" download><button class="btn btn-primary">Resume</button></a>', unsafe_allow_html=True)
-
-    with col3:
-        st.write(f'<a href="{contact_email}"><button class="btn btn-primary">Contact Me</button></a>', unsafe_allow_html=True)
 
     # Additional content moved from the 'about me' section
     st.write("""
@@ -40,6 +23,13 @@ def main():
 
     I built this web app to offer a deeper look into my expertise, showcase the projects I’ve worked on, and connect with other professionals who share similar passions. You’ll find a summary of my professional journey below, and more detailed insights about my roles and projects in the subpages.
     """)
+
+    # Hyperlinks for LinkedIn and Resume
+    linkedin_url = "https://www.linkedin.com/in/puravp"
+    resume_file = 'https://raw.githubusercontent.com/puravpatel3/portfolio/main/files/Purav_Patel_Resume.pdf'
+
+    st.markdown(f'[View my LinkedIn]({linkedin_url})')
+    st.markdown(f'[View my Resume]({resume_file})')
 
     # Professional Summary (with styling for role titles and details)
     st.write("""
