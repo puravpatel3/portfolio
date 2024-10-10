babf876f12ca61d29a2d56ee93c9d2797cd77308
puravpatel3 <166936464+puravpatel3@users.noreply.github.com>
2024-10-10 16:35:41 -0400

    Update 6_Project_Car_Sales.py

--- pages/6_Project_Car_Sales.py
+++ pages/6_Project_Car_Sales.py
@@ -225,7 +225,7 @@ if not filtered_df.empty:
             # Formatting x-axis and y-axis
             ax.set_xlabel('Quarter')
             ax.set_ylabel('Revenue ($)')
-            ax.xaxis.set_major_formatter(mdates.DateFormatter('%YQ%q'))
+            ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-Q%q'))
             plt.xticks(rotation=45)
 
             # Set x-axis limits using datetime objects
